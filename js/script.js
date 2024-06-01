09fcf5efbb84990c46d256f20c633efa64757b08
Mark O Meara <markomeara98@gmail.com>
2024-06-01 17:40:02 +0100

    start of adding in modals

--- js/script.js
+++ js/script.js
@@ -16,8 +16,8 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 
 
 
-// testimonials variables
-const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+// blog post variables
+const blogPostsItem = document.querySelectorAll("[data-blogpost-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
@@ -28,26 +28,30 @@ const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
 
 // modal toggle function
-const testimonialsModalFunc = function () {
+const blogPostModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 }
 
 // add click event to all modal items
-for (let i = 0; i < testimonialsItem.length; i++) {
+for (let i = 0; i < blogPostsItem.length; i++) {
 
-  testimonialsItem[i].addEventListener("click", function () {
+  blogPostsItem[i].addEventListener("click", function () {
 
     modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
     modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+    modalTitle.innerHTML = this.querySelector("[data-blogpost-title]").innerHTML;
+    modalText.innerHTML = this.querySelector("[data-blogpost-text]").innerHTML;
 
-    testimonialsModalFunc();
+    blogPostModalFunc();
 
   });
 
 }
+
+// add click event to modal close button
+modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+overlay.addEventListener("click", testimonialsModalFunc);
 
 
 // custom select variables
