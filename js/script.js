c955200dfe90cf326d038398b5590fa6ec609e56
Mark O Meara <markomeara98@gmail.com>
2024-06-01 18:17:29 +0100

    redoing

--- js/script.js
+++ js/script.js
@@ -16,8 +16,8 @@ sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); }
 
 
 
-// blog post variables
-const blogPostsItem = document.querySelectorAll("[data-blogpost-item]");
+// testimonials variables
+const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
@@ -28,30 +28,28 @@ const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
 
 // modal toggle function
-const blogPostModalFunc = function () {
+const testimonialsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 }
 
 // add click event to all modal items
-for (let i = 0; i < blogPostsItem.length; i++) {
+for (let i = 0; i < testimonialsItem.length; i++) {
 
-  blogPostsItem[i].addEventListener("click", function () {
+  testimonialsItem[i].addEventListener("click", function () {
 
     modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
     modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-    modalTitle.innerHTML = this.querySelector("[data-blogpost-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-blogpost-text]").innerHTML;
+    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
+    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
 
-    blogPostModalFunc();
+    testimonialsModalFunc();
 
   });
 
 }
 
-// add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+
 
 
 // custom select variables
