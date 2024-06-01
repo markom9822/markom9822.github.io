93f6b825f6c079dfc9ae58ecfe45daf9a10659b1
Mark O Meara <markomeara98@gmail.com>
2024-06-01 13:08:37 +0100

    potential fix

--- js/script.js
+++ js/script.js
@@ -75,8 +75,6 @@ const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
 
-  console.log(selectedValue)
-
   for (let i = 0; i < filterItems.length; i++) {
 
     if (selectedValue === "all") {
